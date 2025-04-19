e7e2295abee6b42e88e7ed1ff9fbfb1d8eef9a70
KOSANG YADAV <kosangyadav2004@gmail.com>
2025-04-19 21:02:33 +0530

    feat : add cloudinary function

--- app/api/jsonToUploadFile/route.ts
+++ app/api/jsonToUploadFile/route.ts
@@ -1,40 +1,40 @@
-import { callNebius } from '@/lib/callNebius';
-import { uploadCloudinary } from '@/lib/uploadToCloudinary';
-import fs from 'fs';
-import { NextRequest, NextResponse } from 'next/server';
+import { llmCall } from "@/lib/callLLM";
+import { uploadCloudinary } from "@/lib/uploadToCloudinary";
+import fs from "fs";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
-  try {
-    const { docBase64, filename } = await req.json();
-    // console.log("docbase:", docBase64);
+	try {
+		const { docBase64, filename } = await req.json();
+		// console.log("docbase:", docBase64);
 
-    const { text } = await callNebius(docBase64);
-    console.log('text : ', text);
+		const { text } = await llmCall(docBase64);
+		console.log("text : ", text);
 
-    if (!text) {
-      throw new Error('model call failed...');
-    }
-    // write json text to a temporary file
-    // const filePath = `public/${filename}`;
-    // fs.writeFileSync(filePath, text!);
+		if (!text) {
+			throw new Error("model call failed...");
+		}
+		// write json text to a temporary file
+		const filePath = `public/${filename}`;
+		// fs.writeFileSync(filePath, text!);
 
-    // const uploadResult = await uploadCloudinary(filename, filePath);
+		const uploadResult = await uploadCloudinary(filename, filePath);
 
-    // if (!uploadResult) {
-    // 	throw new Error("File upload failed...");
-    // }
+		if (!uploadResult) {
+			throw new Error("File upload failed...");
+		}
 
-    // fs.unlinkSync(filePath);
+		fs.unlinkSync(filePath);
 
-    return NextResponse.json(
-      { success: true, message: 'File uploaded successfully', text: text },
-      { status: 200 }
-    );
-  } catch (error: any) {
-    console.error(error);
-    return NextResponse.json(
-      { success: false, error: error.message },
-      { status: 500 }
-    );
-  }
+		return NextResponse.json(
+			{ success: true, message: "File uploaded successfully", text: text },
+			{ status: 200 }
+		);
+	} catch (error: any) {
+		console.error(error);
+		return NextResponse.json(
+			{ success: false, error: error.message },
+			{ status: 500 }
+		);
+	}
 }
